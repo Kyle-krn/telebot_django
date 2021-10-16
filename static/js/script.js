8b19221c229c267dbd5e760c7b34ed821ed83fc2
Egor <egorjkee96@gmail.com>
2021-10-16 21:16:43 +0300

    delete folder static

--- static/js/script.js
+++
@@ -1,6 +0,0 @@
-$(window).scroll(function() {
-    if ($(document).scrollTop() > 600 && $("#myModal").attr("displayed") === "false") {
-      $('#myModal').modal('show');
-      $("#myModal").attr("displayed", "true");
-    }
-  });
