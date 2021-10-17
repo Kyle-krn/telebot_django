8e997cd2d12053c63c3d372f64bccf6adde2892a
Egor <egorjkee96@gmail.com>
2021-10-18 02:43:07 +0300

    help text models

--- main_app/models.py
+++ main_app/models.py
@@ -1,21 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import User
-# from django.utils.text import slugify
-from slugify import slugify
 from django.urls import reverse
 from django.utils.timezone import pytz
 import vape_shop.settings as settings
 
 class Category(models.Model):
     '''Модель категорий'''
-    name = models.CharField(max_length=255)
-    photo = models.ImageField(upload_to='category_img/')
-    slug = models.CharField(max_length=255, unique=True, blank=True, null=True)
+    name = models.CharField(max_length=255, help_text='Имя категории')
+    photo = models.ImageField(upload_to='category_img/', help_text='Фото категории')
+    slug = models.CharField(max_length=255, unique=True, blank=True, null=True, help_text='Используется в боте для поиска категории')
     max_count_product = models.IntegerField()
 
     def save(self, *args, **kwargs):
         super(Category, self).save(*args, **kwargs)
-        self.slug = f'c||{self.pk}'
+        self.slug = f'c||{self.pk}' # Слаг id из за ограничений телеграма в callback_data
         super(Category, self).save(*args, **kwargs)
 
     def __str__(self):
@@ -24,10 +21,10 @@ class Category(models.Model):
 
 class SubCategory(models.Model):
     '''Модель подкатегорий'''
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    name = models.CharField(max_length=150, db_index=True)
-    photo = models.ImageField(upload_to='subcategory_img/')
-    slug = models.CharField(max_length=255, unique=True, blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, help_text='Категория подкатегории')
+    name = models.CharField(max_length=150, db_index=True, help_text='Имя подкатегории')
+    photo = models.ImageField(upload_to='subcategory_img/', help_text='Фотоподкатегории')
+    slug = models.CharField(max_length=255, unique=True, blank=True, null=True, help_text='Слаг подкатегории')
 
 
     def save(self, *args, **kwargs):
@@ -42,13 +39,13 @@ class SubCategory(models.Model):
 class Product(models.Model):
     '''Модель товара'''
     title = models.CharField(max_length=255, verbose_name='Название товара')
-    photo = models.ImageField(upload_to='product_img/')
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    count = models.IntegerField(default=0)
-    slug = models.CharField(max_length=255, unique=True, blank=True, null=True)
-    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
-    weight = models.IntegerField()
+    photo = models.ImageField(upload_to='product_img/', help_text='Фото товара')
+    description = models.TextField(help_text='Описание товара')
+    price = models.DecimalField(max_digits=10, decimal_places=2, help_text='Цена товара')
+    count = models.IntegerField(default=0, help_text='Остаток на складе')
+    slug = models.CharField(max_length=255, unique=True, blank=True, null=True, help_text='Слаг товара')
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE, help_text='Подкатегория товара')
+    weight = models.IntegerField(help_text='Вес товара')
 
     def save(self, *args, **kwargs):
         super(Product, self).save(*args, **kwargs)
@@ -64,14 +61,15 @@ class Product(models.Model):
 
 class ReceptionProduct(models.Model):
     '''Модель пополнения кол-ва товара (приемка)'''
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    note = models.CharField(max_length=255, blank=True, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    count = models.IntegerField()
-    date = models.DateTimeField(auto_now_add=True)
-    liquidated = models.BooleanField(default=False) # True для ликвидированного товара
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, help_text='Товар')
+    note = models.CharField(max_length=255, blank=True, null=True, help_text='Заметка приемки')
+    price = models.DecimalField(max_digits=10, decimal_places=2, help_text='Закупочная цена')
+    count = models.IntegerField(help_text='Кол-во товара в приемке')
+    date = models.DateTimeField(auto_now_add=True, help_text='Дата и время приемки')
+    liquidated = models.BooleanField(default=False, help_text='Ликвидация товара') # True для ликвидированного товара
 
     def get_datetime(self):
+        '''Возвращает московское время'''
         user_timezone = pytz.timezone(settings.TIME_ZONE)
         datetime = self.date.astimezone(user_timezone)
         return datetime.strftime('%m/%d/%Y %H:%M')
@@ -85,26 +83,26 @@ class ReceptionProduct(models.Model):
 
 class TelegramUser(models.Model):
     '''Модель юзеров из телеграма'''
-    chat_id = models.IntegerField(unique=True)
-    first_name = models.CharField(max_length=255, blank=True, null=True)
-    last_name = models.CharField(max_length=255, blank=True, null=True)
-    username = models.CharField(max_length=255, blank=True, null=True)
-    fio = models.CharField(max_length=255, blank=True, null=True)
-    address = models.TextField(blank=True, null=True)
-    number = models.BigIntegerField(blank=True, null=True)
-    post_index = models.IntegerField(blank=True, null=True)
-    search_data = models.CharField(max_length=255, blank=True, null=True)
+    chat_id = models.IntegerField(unique=True, help_text='Id пользователя телеграм')
+    first_name = models.CharField(max_length=255, blank=True, null=True, help_text='Имя')
+    last_name = models.CharField(max_length=255, blank=True, null=True, help_text='Фамилия')
+    username = models.CharField(max_length=255, blank=True, null=True, help_text='Юзернейм')
+    fio = models.CharField(max_length=255, blank=True, null=True, help_text='ФИО для доставки')
+    address = models.TextField(blank=True, null=True, help_text='Адрес доставки')
+    number = models.BigIntegerField(blank=True, null=True, help_text='Телефон пользователя')
+    post_index = models.IntegerField(blank=True, null=True, help_text='Почтовый индекс')
+    search_data = models.CharField(max_length=255, blank=True, null=True, help_text='Данные для поиска товара')
 
     def __str__(self):
         return f"{self.chat_id} -- {self.username}"
 
 
 class TelegramProductCartCounter(models.Model):
-    '''Модель для каунтера, используется для клавиатуры добавления в корзину'''
-    user = models.ForeignKey(TelegramUser, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    count = models.IntegerField(default=1)
-    counter = models.BooleanField(default=True)
+    '''Модель для каунтера и корзины, используется для клавиатуры добавления в корзину'''
+    user = models.ForeignKey(TelegramUser, on_delete=models.CASCADE, help_text='Юзер')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, help_text='Товар')
+    count = models.IntegerField(default=1, help_text='Кол-во товара')
+    counter = models.BooleanField(default=True, help_text='Если True, берет значение count для отображения в клавиатуре')
 
     def __str__(self):
         return f"{self.user.username} -- {self.count}"
@@ -131,11 +129,11 @@ class QiwiToken(models.Model):
 ###########################################################################
 class SoldProduct(models.Model):
     '''Модель проданных товаров'''
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=10, decimal_places=2)    
-    count = models.IntegerField()
-    date = models.DateTimeField(auto_now_add=True)
-    payment_bool = models.BooleanField(default=False)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, help_text='Продукт')
+    price = models.DecimalField(max_digits=10, decimal_places=2, help_text='Цена на момент продажи')    
+    count = models.IntegerField(help_text='Кол-во проданного товара')
+    date = models.DateTimeField(auto_now_add=True, help_text='Дата и время продажи')
+    payment_bool = models.BooleanField(default=False, help_text='Произведена ли оплата')
 
     def get_datetime(self):
         user_timezone = pytz.timezone(settings.TIME_ZONE)
@@ -151,17 +149,17 @@ class SoldProduct(models.Model):
 
 class OrderingProduct(models.Model):
     '''Модель заказа'''
-    user = models.ForeignKey(TelegramUser, on_delete=models.CASCADE)
-    delivery_pay = models.IntegerField()
-    sold_product = models.ManyToManyField(SoldProduct)  # <==== тут
-    track_code = models.BigIntegerField(blank=True, null=True)
-    check_admin = models.BooleanField(default=False)
-    datetime = models.DateTimeField(auto_now_add=True)
-    fio = models.CharField(max_length=255, blank=True, null=True)
-    address = models.TextField(blank=True, null=True)
-    number = models.BigIntegerField(blank=True, null=True)
-    post_index = models.BigIntegerField(blank=True, null=True)
-    payment_bool = models.BooleanField(default=False)
+    user = models.ForeignKey(TelegramUser, on_delete=models.CASCADE, help_text='Юзер')
+    delivery_pay = models.IntegerField(help_text='Стоимость доставки')
+    sold_product = models.ManyToManyField(SoldProduct, help_text='Товары в заказе')  # <==== тут
+    track_code = models.BigIntegerField(blank=True, null=True, help_text='Трек-код заказа')
+    check_admin = models.BooleanField(default=False) # Использовалось для qiwi, что то с этим сделать
+    datetime = models.DateTimeField(auto_now_add=True, help_text='Дата и время создания заказа')
+    fio = models.CharField(max_length=255, blank=True, null=True, help_text='ФИО для доставки')     # Дублируется на случай если юзер удалит или поменяет данные для доставки
+    address = models.TextField(blank=True, null=True, help_text='Адрес доставки')
+    number = models.BigIntegerField(blank=True, null=True, help_text='Номер телефона пользователя')
+    post_index = models.BigIntegerField(blank=True, null=True, help_text='Почтовый индекс')
+    payment_bool = models.BooleanField(default=False, help_text='Оплачен ли заказ') # что то с этим сделать
 
 
     def get_order_price(self):
