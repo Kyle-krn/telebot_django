bad1eebd78df33172d8df6f412a13a00cd5da26c
Kyle-krn <83144447+Kyle-krn@users.noreply.github.com>
2022-01-27 15:45:27 +0300

    Update my_templatetag.py

--- online_shop/templatetags/my_templatetag.py
+++ online_shop/templatetags/my_templatetag.py
@@ -9,17 +9,10 @@ def has_group(user, group_name):
     group =  Group.objects.get_or_create(name=group_name)
     return True if group[0] in user.groups.all() else False
 
-<<<<<<< HEAD
-=======
 
->>>>>>> cec2b49b393484ab9534e8be448a6dcc7616b913
 @register.filter(name='markdown_to_html')
 def markdown_to_html(text):
     while '\n' in text:
         text = text.replace('\n', '<br>')
-<<<<<<< HEAD
     return text
 
-=======
-    return text
->>>>>>> cec2b49b393484ab9534e8be448a6dcc7616b913
